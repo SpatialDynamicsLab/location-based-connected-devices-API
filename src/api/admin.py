14d8f6e8563001a0fb3f00ef65e4cfca87887f18
José P. Gómez B. S <jpablo.gomezb@hotmail.com>
2023-05-18 16:59:26 +0100

    Updated settings

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.site_header = "UCD Spatial Dynamics Lab | " \
+                         "Location-based connected devices API"
+
