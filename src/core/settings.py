56b456c9e885865a15ffcd30f72d3a4ecbadd86a
José P. Gómez B. S <jpablo.gomezb@hotmail.com>
2023-05-18 13:25:37 +0100

    Configured staticfiles

--- src/core/settings.py
+++ src/core/settings.py
@@ -21,6 +21,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(BASE_DIR)
 
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
@@ -145,6 +146,7 @@ USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
